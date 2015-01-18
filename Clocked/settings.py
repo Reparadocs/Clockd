5006a651f58cc2e8b9961265d1e6240dfaa53678
Rishab Hegde <reparadocs@pcp046837pcs.wireless.calpoly.edu>
2015-01-18 10:16:53 -0800

    hours and migrate

--- Clocked/settings.py
+++ Clocked/settings.py
@@ -58,7 +58,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'employee.middleware.TimezoneMiddleware',
 )
 
 ROOT_URLCONF = 'Clocked.urls'
